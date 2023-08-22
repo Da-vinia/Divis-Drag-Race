a9fb87a6041345264bfda5914b9ede362aa5f36c
Da-vinia <daviniatosco@gmail.com>
2023-08-22 13:04:52 +0200

    create and move player

--- src/game.js
+++ src/game.js
@@ -7,14 +7,16 @@ class Game {
         this.displayLives = document.querySelector('#lives');
         this.player = new Player (
             this.gameScreen,
-            200,
-            500,
-            100,
-            150,
-            "../images/drags/drag3.png"
+            100, // left
+            0, // top
+            500, // width
+            700, // height
+            "../images/drags/drag3.png",
+            2, // z-index
+            "../audio/RuPaul-supermodel.mp3"
         )
-        this.height = 768; // 40vh
-        this.width = 1000;
+        this.height = "40vh" // 768; 
+        this.width = "100vw";
         this.obstacles = [];
         this.score = 0;
         // "../images/icons/crown.png";
@@ -23,19 +25,19 @@ class Game {
         this.gameOver = false;
     }
 
-    updateLives() {
-        const addLives = document.getElementById("lives");
-        const ul = document.createElement("ul");
-        addLives.appendChild(ul);
+    // updateLives() {
+    //     const addLives = document.getElementById("lives");
+    //     const ul = document.createElement("ul");
+    //     addLives.appendChild(ul);
       
-        addLives.forEach((el) => {
-          const newLi = document.createElement('li');
+    //     addLives.forEach((el) => {
+    //       const newLi = document.createElement('li');
           
-          newLi.innerHTML = el;
-          newLi.className = "live-item";
-          ul.appendChild(newLi);
-        });
-    };
+    //       newLi.innerHTML = el;
+    //       newLi.className = "live-item";
+    //       ul.appendChild(newLi);
+    //     });
+    // };
       
 
     start() {
@@ -43,7 +45,7 @@ class Game {
         this.gameScreen.style.height = `${this.height}px`;
         this.startScreen.style.display = 'none';
         this.gameScreen.style.display = 'flex';
-        this.updateLives();
+        // this.updateLives();
         this.gameLoop();
     }
 
@@ -57,71 +59,68 @@ class Game {
 
     update() {
         this.player.move();
-        if(Math.random() > 0.98 && this.obstacles.length < 8) {
-            const positiveObstacle = Math.random() > 0.5;
-            const obstacleImg = positiveObstacle ? "../images/icons/crown.png" : "../images/icons/gems.png";
-
-            const obstacle = new Obstacle(this.gameScreen, obstacleImg, positiveObstacle);
-            this.obstacles.push(obstacle);
-        }
-
-        for(let i = this.obstacles.length -1; i >= 0; i--) {
-            const obstacle = this.obstacles[i];
-            obstacle.move();
-
-            if(obstacle.top > this.height) {
-                obstacle.element.remove();
-                this.obstacles.splice(i, 1);
-
-                if(obstacle.positiveObstacle) {
-                    this.score++;
-                    this.displayScore.textContent = this.score;
-                } else {
-                    this.lives--;
-                    this.displayLives.textContent = this.lives;
-
-                    if(this.lives <= 0) {
-                        this.endGame()
-                    }
-                }
-            }
-            if(this.player.didCollide(obstacle)) {
-                obstacle.element.remove();
-                this.obstacles.splice(i, 1);
-
-                if(obstacle.positiveObstacle) {
-                    this.score++;
-                    this.displayScore.textContent = this.score;
-                } else {
-                    this.lives--;
-                    this.displayLives.textContent = this.lives;
-
-                    if(this.lives <= 0){
-                        this.endGame();
-                    }
-                }
-            }
-        }    
-    }
-
-    endGame() {
-        this.player.element.remove();
-        this.obstacles.forEach(obstacle => obstacle.element.remove());
         
-        this.gameOver = true;
+    }
+    //     if(Math.random() > 0.98 && this.obstacles.length < 8) {
+    //         const positiveObstacle = Math.random() > 0.5;
+    //         const obstacleImg = positiveObstacle ? "../images/icons/crown.png" : "../images/icons/gems.png";
+
+    //         const obstacle = new Obstacle(this.gameScreen, obstacleImg, positiveObstacle);
+    //         this.obstacles.push(obstacle);
+    //     }
+
+    //     for(let i = this.obstacles.length -1; i >= 0; i--) {
+    //         const obstacle = this.obstacles[i];
+    //         obstacle.move();
+
+    //         if(obstacle.top > this.height) {
+    //             obstacle.element.remove();
+    //             this.obstacles.splice(i, 1);
+
+    //             if(obstacle.positiveObstacle) {
+    //                 this.score++;
+    //                 this.displayScore.textContent = this.score;
+    //             } else {
+    //                 this.lives--;
+    //                 this.displayLives.textContent = this.lives;
+
+    //                 if(this.lives <= 0) {
+    //                     this.endGame()
+    //                 }
+    //             }
+    //         }
+    //         if(this.player.didCollide(obstacle)) {
+    //             obstacle.element.remove();
+    //             this.obstacles.splice(i, 1);
+
+    //             if(obstacle.positiveObstacle) {
+    //                 this.score++;
+    //                 this.displayScore.textContent = this.score;
+    //             } else {
+    //                 this.lives--;
+    //                 this.displayLives.textContent = this.lives;
+
+    //                 if(this.lives <= 0){
+    //                     this.endGame();
+    //                 }
+    //             }
+    //         }
+    //     }    
+    // }
+
+    // endGame() {
+    //     this.player.element.remove();
+    //     this.obstacles.forEach(obstacle => obstacle.element.remove());
         
-        this.gameScreen.style.display = "none";
-        this.endScreen.style.display = "flex";   
-    }
+    //     this.gameOver = true;
+        
+    //     this.gameScreen.style.display = "none";
+    //     this.endScreen.style.display = "flex";   
+    // }
     
-    sumScore() {
+    // sumScore() {
     
-    }
+    // }
 
-
-
-
-
-
-
+    // updateJump() {}
 }
